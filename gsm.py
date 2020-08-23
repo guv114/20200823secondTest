510fc38087c5ce4246298154371820523d4305de
guv114 <68322350+guv114@users.noreply.github.com>
2020-08-23 21:25:17 +0100

    updated gsm.py
    I have now just made a test update and ready to commit to mast after i cloned the orignal file from github

--- gsm.py
+++ gsm.py
@@ -1,1 +1,12 @@
-print("Hello world on github")
+print("Hello you!!")
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+
+if age < 35:
+    print("You are less then 35 years of age")
+elif age == 35:
+    print("You are bang on 35")
+elif age > 35:
+    print("Wow you are old mannn!!")
+else:
+    print("Type in your age not something else!!")
